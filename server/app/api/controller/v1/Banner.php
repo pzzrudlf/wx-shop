40e44f37c5c70a01f88bbe7e11d70a5c9300efd6
pzzrudlf <pzzrudlf@gmail.com>
2020-03-10 15:52:17 +0800

    s

--- server/app/api/controller/v1/Banner.php
+++ server/app/api/controller/v1/Banner.php
@@ -3,12 +3,7 @@
 
 namespace app\api\controller\v1;
 
-use app\common\controller\Common;
-
-//这里的Banner类的全称是"app\api\controller\v1\Banner",不是"Banner"
-//
-//此时，我没有继承ThinkPHP中的控制器类，因为现在我用不到它
-class Banner extends Common
+class Banner extends Base
 {
     //这个是控制器类的方法，
     //是要跟url产生对应关系的
@@ -20,7 +15,6 @@ class Banner extends Common
 
     public function demo()
     {
-        print_r(__CLASS__);echo "<br>";
-        exit($this->test());
+        return false;
     }
 }
--- server/app/extra/upload_local.php
+++ server/app/extra/upload_local.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'img_prefix' => 'http://wx-mini-shop.dev.com/uploads',
+    'img_prefix' => 'http://127.0.0.1:8000/images',
     'token_expire_in' => 7200,
     'token_salt' => 'klidhga oahkjgdf',
     'domain' => 'https://您的域名',
